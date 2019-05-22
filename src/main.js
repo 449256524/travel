bbadfa49472d1614997f180f3aa97659f95a69be
liyu <449256524@qq.com>
2019-05-22 17:57:53 +0700

    首页头部完成

--- src/main.js
+++ src/main.js
@@ -3,8 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick'
+// iconfont图标
+import 'styles/iconfont.css'
+// 重置不同浏览器css样式
+import 'styles/reset.css'
+// 防止不同手机屏幕像素边框不一致
+import 'styles/border.css'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe624;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont arrow-icon">&#xe64a;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~styles/variables.stylus";
+  .header
+    display: flex;
+    line-height: 3rem;
+    background: $bgColor;
+    color: #fff;
+    .header-left
+      width: 3rem;
+      float: left;
+      .back-icon
+        text-align: center;
+        font-size: 1.5rem;
+    .header-input
+      flex: 1;
+      background: white;
+      border-radius: 0.6rem;
+      margin-top: 0.3rem;
+      line-height: 2.4rem;
+      height: 2.4rem;
+      color: #ccc;
+      padding-left: 0.5rem;
+    .header-right
+      width: 4rem;
+      text-align: center;
+      .arrow-icon
+        font-size: 0.8rem;
+        margin-left: -0.3rem;
+</style>
